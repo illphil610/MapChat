746169a8524ca96d9d09f688144aa4efa2388703
Philip <pcappelli901@gmail.com>
2018-03-04 02:18:45 -0500

    Event Bus is sent up and successfully sending data from the MainActivity to the fragment.  Next is to add the MapFragment and have it display the coordinates of each user.

--- app/src/main/java/com/newwesterndev/mapchat/MainActivity.kt
+++ app/src/main/java/com/newwesterndev/mapchat/MainActivity.kt
@@ -4,23 +4,14 @@ import android.app.Activity
 import android.app.FragmentManager
 import android.app.FragmentTransaction
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.newwesterndev.mapchat.Adapter.DataAdapter
 import com.newwesterndev.mapchat.Fragments.PartnerListFragment
 import com.newwesterndev.mapchat.Model.Model
-import com.newwesterndev.mapchat.Model.RxBus
 import com.newwesterndev.mapchat.Network.RequestInterface
 import com.newwesterndev.mapchat.Utilities.Utility
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 class MainActivity : Activity(), DataAdapter.Listener {
 
@@ -34,28 +25,13 @@ class MainActivity : Activity(), DataAdapter.Listener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fragmentManager.inTransaction { add(R.id.mapchat_nav_fragment,
-                PartnerListFragment.newInstance()) }
+        fragmentManager.inTransaction {
+            replace(R.id.mapchat_nav_fragment, PartnerListFragment.newInstance())
+        }
     }
 
-    private fun handleResponse(userList: List<Model.User>) {
-        Toast.makeText(this, "Updating list", Toast.LENGTH_SHORT).show()
-        mUserArrayList = ArrayList(userList)
-        RxBus.publish(Model.UserList(mUserArrayList))
-    }
-
-    private fun handleError(error: Throwable) {
-        Log.d(MainActivity::class.java.simpleName, error.localizedMessage)
-        Toast.makeText(this, "Error ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun pollServer(requestInterface: RequestInterface) {
-        mDisposable = Observable.interval(30, TimeUnit.SECONDS)
-                .startWith(0)
-                .flatMap { requestInterface.getUsers() }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(this::handleResponse, this::handleError)
+    private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        beginTransaction().func().commit()
     }
 
     override fun onItemClick(user: Model.User) {
@@ -63,26 +39,14 @@ class MainActivity : Activity(), DataAdapter.Listener {
                 user.longitude, Toast.LENGTH_SHORT).show()
     }
 
-    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
-        beginTransaction().func().commit()
-    }
-
     override fun onResume() {
         super.onResume()
         mRequestInterface = mUtility.loadJSON()
-        pollServer(mRequestInterface)
+        mDisposable = mUtility.pollServer(mRequestInterface)
     }
 
     override fun onStop() {
         super.onStop()
-        mCompositeDisposable.add(mDisposable)
-        mCompositeDisposable.clear()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        //mUtility.clearDisposables(mCompositeDisposable, mDisposable)
-        mCompositeDisposable.add(mDisposable)
-        mCompositeDisposable.clear()
+        mUtility.clearDisposables(mCompositeDisposable, mDisposable)
     }
 }
--- app/src/main/java/com/newwesterndev/mapchat/Utilities/Utility.kt
+++ app/src/main/java/com/newwesterndev/mapchat/Utilities/Utility.kt
@@ -3,17 +3,22 @@ package com.newwesterndev.mapchat.Utilities
 import android.app.FragmentManager
 import android.app.FragmentTransaction
 import android.content.Context
-import android.provider.Settings.Global.getString
+import android.util.Log
+import android.widget.Toast
+import com.newwesterndev.mapchat.MainActivity
 import com.newwesterndev.mapchat.Model.Model
 import com.newwesterndev.mapchat.Model.RxBus
 import com.newwesterndev.mapchat.Network.RequestInterface
 import com.newwesterndev.mapchat.R
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.security.AccessControlContext
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by philip on 3/1/18.
@@ -21,22 +26,38 @@ import java.security.AccessControlContext
 class Utility(context: Context) {
 
     val mContext = context
+    private var mUserArrayList: ArrayList<Model.User> = ArrayList()
 
     fun clearDisposables(compositeDisposable: CompositeDisposable, disposable: Disposable) {
         compositeDisposable.add(disposable)
         compositeDisposable.clear()
     }
 
-    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
-        beginTransaction().func().commit()
-    }
-
     fun loadJSON(): RequestInterface {
-        val requestInterface = Retrofit.Builder()
+        return Retrofit.Builder()
                 .baseUrl(mContext.getString(R.string.kamorris))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build().create(RequestInterface::class.java)
-        return requestInterface
+    }
+
+    fun pollServer(requestInterface: RequestInterface) : Disposable {
+        return Observable.interval(30, TimeUnit.SECONDS)
+                .startWith(0)
+                .flatMap { requestInterface.getUsers() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse, this::handleError)
+    }
+
+    private fun handleResponse(userList: List<Model.User>) {
+        Toast.makeText(mContext, "Updating list", Toast.LENGTH_SHORT).show()
+        mUserArrayList = ArrayList(userList)
+        RxBus.publish(Model.UserList(mUserArrayList))
+    }
+
+    private fun handleError(error: Throwable) {
+        Log.d(MainActivity::class.java.simpleName, error.localizedMessage)
+        Toast.makeText(mContext, "Error ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
     }
 }
