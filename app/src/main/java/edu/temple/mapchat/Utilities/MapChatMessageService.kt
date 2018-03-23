19b5178cd775ed9c94bc3f88cd63f0d6654de010
Philip <pcappelli901@gmail.com>
2018-03-23 00:14:02 -0400

    Successfully posting to karls server with FCM id....at least I think so.  I got a success return but I cant verify on his end.

--- app/src/main/java/edu/temple/mapchat/Utilities/MapChatMessageService.kt
+++ app/src/main/java/edu/temple/mapchat/Utilities/MapChatMessageService.kt
@@ -4,26 +4,39 @@ import android.content.ContentValues.TAG
 import android.util.Log
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.iid.FirebaseInstanceIdService
+import retrofit2.Call
+import retrofit2.Response
 
 /**
  * Created by philip on 3/21/18.
  */
 class MapChatMessageService: FirebaseInstanceIdService() {
 
-    //val mUtility = Utility(applicationContext)
-    var hasTokenBeenSentToKarlsServer: Boolean = false
+    private var hasTokenBeenSentToKarlsServer: Boolean = false
+    private val mUtility = Utility(this)
 
-    /*
     override fun onTokenRefresh() {
         super.onTokenRefresh()
         val refreshedToken = FirebaseInstanceId.getInstance().token
         Log.e(TAG, "Refreshed token: $refreshedToken")
 
         // send id to karls server
+        // this if statement is dumb
+        /*
         if (!hasTokenBeenSentToKarlsServer) {
             val requestInterface = mUtility.loadJSON()
-            refreshedToken?.let { requestInterface.addUserToken("Phil", it) }
+            refreshedToken?.let {
+                requestInterface.addUserToken("Phil", it)
+                    .enqueue(object : retrofit2.Callback<Void> {
+                        override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
+                            Log.e("POST", "YAY $it")
+                        }
+                        override fun onFailure(call: Call<Void>?, t: Throwable?) {
+                            Log.e("POST", "NO")
+                        }
+                    })
+            }
         }
+        */
     }
-    */
 }
